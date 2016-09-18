cf6b434d7fea298824354d4a740fa16fc4e158b9
stealdre <paulbeneteau@hotmail.fr>
2016-09-18 02:05:53 +0200

    ss

--- Go-Pokedex/AppDelegate.swift
+++ Go-Pokedex/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
        // set white status bar
         
         return true
