c55bae75e356057aa9bae788d894ca8f6fba2e70
stealdre <paulbeneteau@hotmail.fr>
2016-09-19 15:39:05 +0200

    set up About us View A few warnings corrected

--- Go-Pokedex/csvParsing.swift
+++ Go-Pokedex/csvParsing.swift
@@ -48,6 +48,7 @@ public class CSVReader {
         var i = 0
         for line in _lines {
             if line.isEmpty {
+                
                 _lines.removeAtIndex(i++)
             }
             i += 1
